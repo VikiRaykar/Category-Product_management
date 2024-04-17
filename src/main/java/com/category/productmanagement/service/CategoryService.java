55003194e513614b5d772b0f1f4240ff149edb9e
pratikb9696 <bhosalepratik9696@gmail.com>
2024-04-17 23:52:31 +0530

    first commit

---
+++ src/main/java/com/category/productmanagement/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.category.productmanagement.service;
+
+import com.category.productmanagement.pojo.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+     List<Category> getAllCategories();
+     Category addCategory(Category category);
+     Category getCategoryById(Long id);
+     Category updateCategory(Category category, Long id);
+     String deleteCategoryById(Long id);
+}
